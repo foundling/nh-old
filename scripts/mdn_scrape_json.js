7ada062fd09c0fe3e23ef4ef651df4f67d0c6dd4
Kasim Siddiqui <kasimsiddiqui@gmail.com>
2015-09-22 09:08:20 -0700

    object scrape done

---
+++ scripts/mdn_scrape_json.js
@@ -0,0 +1,181 @@
+{
+    "Object": {
+        "description" : "The Object constructor creates an object wrapper for the given value. If the value is null or undefined, it will create and return an empty object, otherwise, it will return an object of a Type that corresponds to the given value. If the value is an object already, it will return the value.
+
+            When called in a non-constructor context, Object behaves identically to new Object().",
+        "properties" : {
+            "Object.length" : "Has a value of 1.",
+            "Object.prototype" : "Allows the addition of properties to all objects of type Object."
+        },
+        "methods" : {
+            "Object.assign()" : "Creates a new object by copying the values of all enumerable own properties from one or more source objects to a target object.",
+            "Object.create()" : "Creates a new object with the specified prototype object and properties.",
+            "Object.defineProperty()" : "Adds the named property described by a given descriptor to an object.",
+            "Object.defineProperties()" : "Adds the named properties described by the given descriptors to an object.",
+            "Object.freeze()" : "Freezes an object: other code can't delete or change any properties.",
+            "Object.getOwnPropertyDescriptor()" : "Returns a property descriptor for a named property on an object.",
+            "Object.getOwnPropertyNames()" : "Returns an array containing the names of all of the given object's own enumerable and non-enumerable properties.",
+            "Object.getOwnPropertySymbols()" : "Returns an array of all symbol properties found directly upon a given object.",
+            "Object.getPrototypeOf()" : "Returns the prototype of the specified object.",
+            "Object.is()" : "Compares if two values are distinguishable (ie. the same)",
+            "Object.isExtensible()" : "Determines if extending of an object is allowed.",
+            "Object.isFrozen()" : "Determines if an object was frozen.",
+            "Object.isSealed()" : "Determines if an object is sealed.",
+            "Object.keys()" : "Returns an array containing the names of all of the given object's own enumerable properties.",
+            "Object.observe()" : "Asynchronously observes changes to an object.",
+            "Object.getNotifier" : "Get a notifier with which to create object changes manually.",
+            "Object.preventExtensions" : "Prevents any extensions of an object.",
+            "Object.seal" : "Prevents other code from deleting properties of an object.",
+            "Object.setPrototypeOf" : "Sets the prototype (i.e., the internal [[Prototype]] property)"
+        }
+    },
+    "String": {
+        "description" : "Strings are useful for holding data that can be represented in text form. Some of the most-used operations on strings are to check their length, to build and concatenate them using the + and += string operators, checking for the existence or location of substrings with the indexOf() method, or extracting substrings with the substring() method.",
+        "properties" : {
+            "String.prototype" : "Allows the addition of properties to a String object.",
+            "String.prototype.constructor" : "Specifies the function that creates an object's prototype.",
+            "String.prototype.length" : "Reflects the length of the string."
+        },
+        "methods" : {
+            "String.fromCharCode()" : "Returns a string created by using the specified sequence of Unicode values.",
+            "String.fromCodePoint() " : "Returns a string created by using the specified sequence of code points.",
+            "String.raw()" : "Returns a string created from a raw template string.",
+            "Object.defineProperties()" : "Adds the named properties described by the given descriptors to an object.",
+            "Object.freeze()" : "Freezes an object: other code can't delete or change any properties.",
+            "Object.getOwnPropertyDescriptor()" : "Returns a property descriptor for a named property on an object.",
+            "Object.getOwnPropertyNames()" : "Returns an array containing the names of all of the given object's own enumerable and non-enumerable properties.",
+            "Object.getOwnPropertySymbols()" : "Returns an array of all symbol properties found directly upon a given object.",
+            "Object.getPrototypeOf()" : "Returns the prototype of the specified object.",
+            "Object.is()" : "Compares if two values are distinguishable (ie. the same)",
+            "Object.isExtensible()" : "Determines if extending of an object is allowed.",
+            "Object.isFrozen()" : "Determines if an object was frozen.",
+            "Object.isSealed()" : "Determines if an object is sealed.",
+            "Object.keys()" : "Returns an array containing the names of all of the given object's own enumerable properties.",
+            "Object.observe()" : "Asynchronously observes changes to an object.",
+            "Object.getNotifier" : "Get a notifier with which to create object changes manually.",
+            "Object.preventExtensions" : "Prevents any extensions of an object.",
+            "Object.seal" : "Prevents other code from deleting properties of an object.",
+            "Object.setPrototypeOf" : "Sets the prototype (i.e., the internal [[Prototype]] property)"
+        }
+    },
+    "Array": {
+        "description" : "The Object constructor creates an object wrapper for the given value. If the value is null or undefined, it will create and return an empty object, otherwise, it will return an object of a Type that corresponds to the given value. If the value is an object already, it will return the value.
+
+            When called in a non-constructor context, Object behaves identically to new Object().",
+        "properties" : {
+            "Object.length" : "Has a value of 1.",
+            "Object.prototype" : "Allows the addition of properties to all objects of type Object."
+        },
+        "methods" : {
+            "Object.assign() " : "Creates a new object by copying the values of all enumerable own properties from one or more source objects to a target object.",
+            "Object.create()" : "Creates a new object with the specified prototype object and properties.",
+            "Object.defineProperty()" : "Adds the named property described by a given descriptor to an object.",
+            "Object.defineProperties()" : "Adds the named properties described by the given descriptors to an object.",
+            "Object.freeze()" : "Freezes an object: other code can't delete or change any properties.",
+            "Object.getOwnPropertyDescriptor()" : "Returns a property descriptor for a named property on an object.",
+            "Object.getOwnPropertyNames()" : "Returns an array containing the names of all of the given object's own enumerable and non-enumerable properties.",
+            "Object.getOwnPropertySymbols()" : "Returns an array of all symbol properties found directly upon a given object.",
+            "Object.getPrototypeOf()" : "Returns the prototype of the specified object.",
+            "Object.is()" : "Compares if two values are distinguishable (ie. the same)",
+            "Object.isExtensible()" : "Determines if extending of an object is allowed.",
+            "Object.isFrozen()" : "Determines if an object was frozen.",
+            "Object.isSealed()" : "Determines if an object is sealed.",
+            "Object.keys()" : "Returns an array containing the names of all of the given object's own enumerable properties.",
+            "Object.observe()" : "Asynchronously observes changes to an object.",
+            "Object.getNotifier" : "Get a notifier with which to create object changes manually.",
+            "Object.preventExtensions" : "Prevents any extensions of an object.",
+            "Object.seal" : "Prevents other code from deleting properties of an object.",
+            "Object.setPrototypeOf" : "Sets the prototype (i.e., the internal [[Prototype]] property)"
+        }
+    },
+    "Function": {
+        "description" : "The Object constructor creates an object wrapper for the given value. If the value is null or undefined, it will create and return an empty object, otherwise, it will return an object of a Type that corresponds to the given value. If the value is an object already, it will return the value.
+
+            When called in a non-constructor context, Object behaves identically to new Object().",
+        "properties" : {
+            "Object.length" : "Has a value of 1.",
+            "Object.prototype" : "Allows the addition of properties to all objects of type Object."
+        },
+        "methods" : {
+            "Object.assign() " : "Creates a new object by copying the values of all enumerable own properties from one or more source objects to a target object.",
+            "Object.create()" : "Creates a new object with the specified prototype object and properties.",
+            "Object.defineProperty()" : "Adds the named property described by a given descriptor to an object.",
+            "Object.defineProperties()" : "Adds the named properties described by the given descriptors to an object.",
+            "Object.freeze()" : "Freezes an object: other code can't delete or change any properties.",
+            "Object.getOwnPropertyDescriptor()" : "Returns a property descriptor for a named property on an object.",
+            "Object.getOwnPropertyNames()" : "Returns an array containing the names of all of the given object's own enumerable and non-enumerable properties.",
+            "Object.getOwnPropertySymbols()" : "Returns an array of all symbol properties found directly upon a given object.",
+            "Object.getPrototypeOf()" : "Returns the prototype of the specified object.",
+            "Object.is()" : "Compares if two values are distinguishable (ie. the same)",
+            "Object.isExtensible()" : "Determines if extending of an object is allowed.",
+            "Object.isFrozen()" : "Determines if an object was frozen.",
+            "Object.isSealed()" : "Determines if an object is sealed.",
+            "Object.keys()" : "Returns an array containing the names of all of the given object's own enumerable properties.",
+            "Object.observe()" : "Asynchronously observes changes to an object.",
+            "Object.getNotifier" : "Get a notifier with which to create object changes manually.",
+            "Object.preventExtensions" : "Prevents any extensions of an object.",
+            "Object.seal" : "Prevents other code from deleting properties of an object.",
+            "Object.setPrototypeOf" : "Sets the prototype (i.e., the internal [[Prototype]] property)"
+        }
+    },
+    "Number": {
+        "description" : "The Object constructor creates an object wrapper for the given value. If the value is null or undefined, it will create and return an empty object, otherwise, it will return an object of a Type that corresponds to the given value. If the value is an object already, it will return the value.
+
+            When called in a non-constructor context, Object behaves identically to new Object().",
+        "properties" : {
+            "Object.length" : "Has a value of 1.",
+            "Object.prototype" : "Allows the addition of properties to all objects of type Object."
+        },
+        "methods" : {
+            "Object.assign() " : "Creates a new object by copying the values of all enumerable own properties from one or more source objects to a target object.",
+            "Object.create()" : "Creates a new object with the specified prototype object and properties.",
+            "Object.defineProperty()" : "Adds the named property described by a given descriptor to an object.",
+            "Object.defineProperties()" : "Adds the named properties described by the given descriptors to an object.",
+            "Object.freeze()" : "Freezes an object: other code can't delete or change any properties.",
+            "Object.getOwnPropertyDescriptor()" : "Returns a property descriptor for a named property on an object.",
+            "Object.getOwnPropertyNames()" : "Returns an array containing the names of all of the given object's own enumerable and non-enumerable properties.",
+            "Object.getOwnPropertySymbols()" : "Returns an array of all symbol properties found directly upon a given object.",
+            "Object.getPrototypeOf()" : "Returns the prototype of the specified object.",
+            "Object.is()" : "Compares if two values are distinguishable (ie. the same)",
+            "Object.isExtensible()" : "Determines if extending of an object is allowed.",
+            "Object.isFrozen()" : "Determines if an object was frozen.",
+            "Object.isSealed()" : "Determines if an object is sealed.",
+            "Object.keys()" : "Returns an array containing the names of all of the given object's own enumerable properties.",
+            "Object.observe()" : "Asynchronously observes changes to an object.",
+            "Object.getNotifier" : "Get a notifier with which to create object changes manually.",
+            "Object.preventExtensions" : "Prevents any extensions of an object.",
+            "Object.seal" : "Prevents other code from deleting properties of an object.",
+            "Object.setPrototypeOf" : "Sets the prototype (i.e., the internal [[Prototype]] property)"
+        }
+    },
+    "Boolean": {
+        "description" : "The Object constructor creates an object wrapper for the given value. If the value is null or undefined, it will create and return an empty object, otherwise, it will return an object of a Type that corresponds to the given value. If the value is an object already, it will return the value.
+
+            When called in a non-constructor context, Object behaves identically to new Object().",
+        "properties" : {
+            "Object.length" : "Has a value of 1.",
+            "Object.prototype" : "Allows the addition of properties to all objects of type Object."
+        },
+        "methods" : {
+            "Object.assign() " : "Creates a new object by copying the values of all enumerable own properties from one or more source objects to a target object.",
+            "Object.create()" : "Creates a new object with the specified prototype object and properties.",
+            "Object.defineProperty()" : "Adds the named property described by a given descriptor to an object.",
+            "Object.defineProperties()" : "Adds the named properties described by the given descriptors to an object.",
+            "Object.freeze()" : "Freezes an object: other code can't delete or change any properties.",
+            "Object.getOwnPropertyDescriptor()" : "Returns a property descriptor for a named property on an object.",
+            "Object.getOwnPropertyNames()" : "Returns an array containing the names of all of the given object's own enumerable and non-enumerable properties.",
+            "Object.getOwnPropertySymbols()" : "Returns an array of all symbol properties found directly upon a given object.",
+            "Object.getPrototypeOf()" : "Returns the prototype of the specified object.",
+            "Object.is()" : "Compares if two values are distinguishable (ie. the same)",
+            "Object.isExtensible()" : "Determines if extending of an object is allowed.",
+            "Object.isFrozen()" : "Determines if an object was frozen.",
+            "Object.isSealed()" : "Determines if an object is sealed.",
+            "Object.keys()" : "Returns an array containing the names of all of the given object's own enumerable properties.",
+            "Object.observe()" : "Asynchronously observes changes to an object.",
+            "Object.getNotifier" : "Get a notifier with which to create object changes manually.",
+            "Object.preventExtensions" : "Prevents any extensions of an object.",
+            "Object.seal" : "Prevents other code from deleting properties of an object.",
+            "Object.setPrototypeOf" : "Sets the prototype (i.e., the internal [[Prototype]] property)"
+        }
+    }
+}
