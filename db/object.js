e4958cc53a1e54748723d6d4d2e96b079477ec9f
Alex Ramsdell <alexramsdell@gmail.com>
2015-09-22 15:54:21 -0700

    some serious progress was made. node and native documents are accessible via the node-help repl

--- db/object.js
+++
@@ -1,3 +0,0 @@
-{"object":
-    {"description":"an object"}
-}
--- stream.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs'); 
-
-fs.readFile('stream', function(err, data) {
-  data = JSON.parse(data.toString());
-  console.log(Object.keys(data));
-  console.dir(data.modules[0].desc);
-});
