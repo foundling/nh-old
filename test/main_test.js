d17b1d99b6edaeac4340e865d129cf8bee9894e3
Stan Zhao <zhaostan09@gmail.com>
2015-09-23 15:03:59 -0700

    adding main  test

---
+++ test/main_test.js
@@ -0,0 +1,22 @@
+var expect = require("chai").expect;
+var main = require(__dirname + "path to main");
+
+describe("printHeader", function(){
+  it("should print header in repl", function(){
+    //write tests
+  });
+});
+
+describe("denest", function(){
+  it("should return string", function(){
+    //write test
+  });
+});
+
+describe("lookup", function(){
+  it("should look up token and return string by calling denest", function(){
+    //write test
+  });
+});
+
+
