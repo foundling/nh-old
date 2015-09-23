e08b6a08aeef931d1fbdfaff1493396727a5b266
Alex Ramsdell <alexramsdell@gmail.com>
2015-09-23 16:12:13 -0700

    refactored main.js into app.js and calling it from index.js in dir root

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var repl = require('repl');
+var app = require('./lib/app');
+app.run();
 
-var r = repl.start({
-  prompt: "> ",
-  useGlobal: true,
-});
-
-r.context.c = function() {
-  for (var i = 0; i < arguments.length; i++) {
-      console.log("_" + i + " = " + arguments[i]);
-      global["_" + i] = arguments[i];
-    }
-};
