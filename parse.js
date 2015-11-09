992ee7a8df091305fdfdac4324031e9e6a56bc49
Alex Ramsdell <alexramsdell@gmail.com>
2015-11-08 18:31:17 -0800

    parse properties and methods for both constructor and prototype, for each global object

--- parse.js
+++ parse.js
@@ -2,7 +2,7 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var objectDB = {};
 var DOCSPATH = 'docs';
-var objFormat = require('./lib/o_format');
+var objFormat = require('./lib/obj_format');
 var testing = true;
 
 // we have files, so parse each one, and add to global 
@@ -12,6 +12,7 @@ fs.readdir(DOCSPATH, function(err, files){
   // for testing single topics
   if (testing) files = [
     'RegExp',
+    'Array',
   ]; 
 
   files.forEach(function(filename){
@@ -21,18 +22,44 @@ fs.readdir(DOCSPATH, function(err, files){
         var $ = cheerio.load(html);
         var description = $('#Description').nextUntil('h2,h3').text(); 
         var syntax = $('.syntaxbox').text();
-        var properties = {};
+
+        var constructorProperties = {};
         $('#Properties').nextUntil('h2,h3','dl').find('dt').each(function(i,el) {
           var propertyName = $(el).text();
-          var description = $(el).next().text();
-          properties[propertyName] = description;
-        }.bind(this));
+          var propertyDescription = $(el).next().text();
+          constructorProperties[propertyName] = propertyDescription;
+        });
+
+        var constructorMethods = {};
+        $('#Methods').nextUntil('h2,h3','dl').find('dt').each(function(i,el) {
+          var methodName = $(el).text();
+          var methodDescription = $(el).next().text();
+          constructorMethods[methodName] = methodDescription;
+        });
+
+        // different structure than the constructor info
+        var prototypeProperties = {};
+        $('#Properties_2').next().find('dl').find('dt').each(function(i, el) {
+          var propertyName = $(el).text();
+          var propertyDescription = $(el).next().text();
+          prototypeProperties[propertyName] = propertyDescription;
+        });
+
+        var prototypeMethods = {};
+        $('#Methods_2').next().find('dl').find('dt').each(function(i, el) {
+          var methodName = $(el).text();
+          var methodDescription = $(el).next().text();
+          prototypeMethods[methodName] = methodDescription;
+        });
 
         // add all of this parsed to the objectDB 
         objectDB[filename] = {
           description: description,
           syntax: syntax,
-          properties: properties,
+          prototypeProperties: prototypeProperties,
+          prototypeMethods: prototypeMethods,
+          constructorProperties: constructorProperties,
+          constructorMethods: constructorMethods,
         };
 
         // then write it out as json
