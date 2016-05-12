8ff17bfac3486e29695a7ea6f4c678c300e85012
Alex Ramsdell <alex.ramsdell@tradetech.net>
2016-05-11 23:45:38 -0700

    added some branching to handle methods and modules traversal. also parameterized the key to look up in recursive descent.

--- server/build/nodejs_docs/menu.js
+++ server/build/nodejs_docs/menu.js
@@ -2,22 +2,27 @@ var fs = require('fs');
 var docs = JSON.parse(fs.readFileSync('./docs.json'));
 var skipList = ['Addons'];
 
-function traverseMenu(root, indent) {
+function traverseMenu(root, targetKey, indent) {
 
+    console.log('[' + targetKey + ']');
     // for each module in 'modules'
-    root.modules.forEach(function(module) {
+    root[targetKey].forEach(function(targetObj) {
+
         // check that the item's not in our skip list by searching it for the obj's textRaw property 
-        if ( module.textRaw && skipList.indexOf(module.textRaw) !== -1 ) {
+        if ( targetObj.textRaw && skipList.indexOf(targetObj.textRaw) !== -1 ) {
             return false;
         }
+        
         // add indent and print it out
-        console.log(indent + module.textRaw);
-        if ('modules' in module) {
-            return traverseMenu(module, indent + '  ');
+        console.log(indent + targetObj.textRaw);
+        if ('modules' in targetObj) {
+            return traverseMenu(targetObj, 'modules', indent + '  ');
+        }
+        if ('methods' in targetObj) {
+            return traverseMenu(targetObj, 'methods', indent + '  ');
         }
     });
 
 };
 
-traverseMenu(docs, '');
-
+traverseMenu(docs, 'modules', '');
